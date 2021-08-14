57dafde997220a2c6f21afd9567f264430275735
areckkimo <smalltanmon@gmail.com>
2021-08-14 12:41:53 +0800

    Initial Commit

---
+++ metropiaPreTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  metropiaPreTest
+//
+//  Created by Eric Chen on 2021/8/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
